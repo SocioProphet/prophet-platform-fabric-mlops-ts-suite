930321a89df0badb55d6be0dc88f86c9672001f9
mdheller <21163552+mdheller@users.noreply.github.com>
2026-06-05 17:43:21 -0400

    Remove Google Workspace provider from default mesh root

--- infra/google-workspace-ops-mesh/providers.tf
+++ infra/google-workspace-ops-mesh/providers.tf
@@ -1,10 +1,3 @@
 provider "google" {
   project = var.google_project_id != "" ? var.google_project_id : null
 }
-
-provider "googleworkspace" {
-  customer_id                 = var.google_workspace_customer_id != "" ? var.google_workspace_customer_id : null
-  impersonated_user_email    = var.google_workspace_impersonated_user_email != "" ? var.google_workspace_impersonated_user_email : null
-  credentials                = var.google_workspace_credentials_file != "" ? file(var.google_workspace_credentials_file) : null
-  oauth_scopes               = var.google_workspace_oauth_scopes
-}
